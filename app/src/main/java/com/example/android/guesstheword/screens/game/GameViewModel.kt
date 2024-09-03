7bf2b1bc4cf5f55d27f55f927e0a1397d6c519ca
Karishma Gupta <karishmaneha42@gmail.com>
2024-09-03 18:39:36 +0530

    Add LiveData

---
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -0,0 +1,73 @@
+package com.example.android.guesstheword.screens.game
+
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GameViewModel:ViewModel() {
+
+    var word = MutableLiveData<String>()
+
+    // The current score
+    var score = MutableLiveData<Int>()
+
+    // The list of words - the front of the list is the next word to guess
+    private lateinit var wordList: MutableList<String>
+
+    init {
+        Log.i("GameViewModel", "GameViewModel Created")
+        resetList()
+        nextWord()
+        word.value=""
+        score.value=0
+    }
+
+
+    private fun resetList() {
+        wordList = mutableListOf(
+            "queen",
+            "hospital",
+            "basketball",
+            "cat",
+            "change",
+            "snail",
+            "soup",
+            "calendar",
+            "sad",
+            "desk",
+            "guitar",
+            "home",
+            "railway",
+            "zebra",
+            "jelly",
+            "car",
+            "crow",
+            "trade",
+            "bag",
+            "roll",
+            "bubble"
+        )
+        wordList.shuffle()
+    }
+
+
+        private fun nextWord() {
+            //Select and remove a word from the list
+            if (wordList.isEmpty()) {
+//                gameFinished()
+            } else {
+                word.value = wordList.removeAt(0)
+            }
+        }
+
+        fun onSkip() {
+            score.value = (score.value)?.minus(1)
+            nextWord()
+        }
+
+        fun onCorrect() {
+            score.value = (score.value)?.plus(1)
+            nextWord()
+        }
+    }
+
